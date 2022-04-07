6bf865805c286f933cc714810a8e4efa518b93c8
PipenkoMikhail <pipenko_mikhail@mail.ru>
2022-04-07 23:13:36 +0300

    Logic

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+
+let a = ""; //first number
+let b = ""; //second number
+let sign = "";//знак операции
+let finish = false;
+
+const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const action = ["+", "-", "*", "/", "%"];
+const out = document.querySelector(".outputField");
+
+
 let num1 = Number(document.getElementByClassName('calculator-button'));
 alert(num1);
