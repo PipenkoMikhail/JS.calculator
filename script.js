101caf14bb9cb9e5f6c67aebfe731c27eefaa521
PipenkoMikhail <pipenko_mikhail@mail.ru>
2022-04-19 20:04:37 +0300

    Add Field

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let a = ""; //first number
 let b = ""; //second number
 let sign = "";//знак операции
@@ -8,6 +7,14 @@ const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 const action = ["+", "-", "*", "/", "%"];
 const out = document.querySelector(".outputField");
 
+function allClear () {
+    let a = ""; //first number
+    let b = ""; //second number
+    let sign = "";//знак операции
+    let finish = false;
+    out.textContent = "0";
+}
+document.querySelector('.AC').onclick = allClear(); 
 
-let num1 = Number(document.getElementByClassName('calculator-button'));
-alert(num1);
+// let num1 = Number(document.getElementByClassName('calculator-button'));
+// alert(num1);
