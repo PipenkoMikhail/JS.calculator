e68157f7ed78ba314bda0a223257ac75017703eb
PipenkoMikhail <pipenko_mikhail@mail.ru>
2022-04-21 16:04:41 +0300

    inter variable

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let a = " "; //first number
-let b = " "; //second number
-let sign = " ";//знак операции
+let a = ""; //first number
+let b = ""; //second number
+let sign = "";//знак операции
 let finish = false;
 
 const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
@@ -14,17 +14,33 @@ function allClear() {
     let finish = false;
     out.textContent = 0;
 }
-document.querySelector('.AC').onclick = allClear;
+
 document.querySelector('.calculator').onclick = (event) => {
     if(!event.target.classList.contains('calculator-button')) return; 
-    if(event.target.classList.contains('AC')) return;
+    if(event.target.classList.contains('.AC')) allClear;
 
     out.textContent = "";
     const clickButton = event.target.textContent;
     
-    if(digit.includes(clickButton)) {
-        a += clickButton;
-        console.log(a, b, sign);
-        out.textContent = a;
+    //проверка нажатия числового значения либо .
+    if (digit.includes(clickButton)) {
+        if (b === '' && sign === '') {
+            a += clickButton;
+            out.textContent = a; 
+        } else if(a!=='' && b!=='' && finish) {
+
+        } else {
+            b += clickButton;
+            out.textContent = b;
+        }
+        console.table(a, b, sign);
+        return;
+    }
+
+    if(action.includes(clickButton)) {
+        sign = clickButton;
+        console.table(a, b, sign);
+        out.textContent = sign;
+        return;
     }
 }
