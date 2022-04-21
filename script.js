6c7384f788ac491b1e19a48c76d557e694dea47c
PipenkoMikhail <pipenko_mikhail@mail.ru>
2022-04-21 10:22:49 +0300

    Find bug

--- script.js
+++ script.js
@@ -7,17 +7,18 @@ const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 const action = ["+", "-", "*", "/", "%"];
 const out = document.querySelector(".outputField");
 
-function allClear () {
+function allClear() {
     let a = ""; //first number
     let b = ""; //second number
     let sign = "";//знак операции
     let finish = false;
     out.textContent = 0;
 }
-document.querySelector('.AC').onclick = allClear();
-document.getElementByClassName('.calculator-button').onclick = (event) {
+document.querySelector('.AC').onclick = allClear;
+document.querySelector('.calculator').onclick = (event) => {
     if(!event.target.classList.contains('calculator-button')) return; 
     if(event.target.classList.contains('AC')) return;
+
     out.textContent = "";
     const clickButton = event.target.textContent;
     
