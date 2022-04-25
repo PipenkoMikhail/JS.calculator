23d026be5c7e905a43a22a42fbd5dd352cfea542
PipenkoMikhail <pipenko_mikhail@mail.ru>
2022-04-25 19:58:18 +0300

    Finish main part of Calculator

--- script.js
+++ script.js
@@ -8,61 +8,69 @@ const action = ["+", "-", "*", "/", "%"];
 const out = document.querySelector(".outputField");
 
 function allClear() {
-    let a = ""; //first number
-    let b = ""; //second number
-    let sign = "";//знак операции
-    let finish = false;
+    a = ""; //first number
+    b = ""; //second number
+    sign = ""; //знак операции
+    // finish = false;
     out.textContent = 0;
-}
-
+};
+//устанавливаем отслеживание события нажатия
 document.querySelector('.calculator').onclick = (event) => {
     if(!event.target.classList.contains('calculator-button')) return; 
     if(event.target.classList.contains('.AC')) allClear;
 
     out.textContent = "";
+    //указываем что в переменной будет значение, взятое из div-а
     const clickButton = event.target.textContent;
     
     //проверка нажатия числового значения либо .
     if (digit.includes(clickButton)) {
-        if (b === '' && sign === '') {
-            a += clickButton;
+        if (b === '' && sign === '') {  //если b и знак пусты
+            a += clickButton;           //продолжаем наполнять а
             out.textContent = a; 
-        } else if(a!=='' && b!=='' && finish) {
-            b = clickButton;
+        } else if(a!=='' && b!=='' && finish) {     //если а и b не пустые, и finish = true
+            b = clickButton;                        //наполнять снова b
             finish = false;
             out.textContent = b;
-        } else {
+        } else {                                    //продолжать наполнять b
             b += clickButton;
             out.textContent = b;
         }
         console.table(a, b, sign);
         return;
     }
-
+//проверка нажатия функциональных кнопок
     if(action.includes(clickButton)) {
         sign = clickButton;
         console.table(a, b, sign);
         out.textContent = sign;
         return;
     }
-
+//логика вычислений калькулятора
     if(clickButton == "=") {
+        if (b === '') b = a;
         switch(sign) {
             case "+": 
                 a = (+a) + (+b);
                 break;
             case "*": 
-                a = (+a) * (+b)
+                a = (+a) * (+b);
                 break;
             case "-": 
-                a = (+a) - (+b)
+                a = (+a) - (+b);
                 break;
-            case "/": 
-                a = (+a) / (+b)
+            case "/":
+                if(b === '0') {
+                    out.textContent = "Ошибка";
+                    a = b = sign = '';
+                    return;
+                } 
+                a = (+a) / (+b);
                 break;
         }
-        out.textContent = a;
-        console.table(a, b, sigh);
         finish = true;
+        out.textContent = a;
+        // console.table(a, b, sigh);
+        
     }
 }
